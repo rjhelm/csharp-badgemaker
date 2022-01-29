8b6e6f91bdb4408758031cd62f68cf9fee596bdc
rjhelm <rjhelm@github.io>
2022-01-29 12:10:48 -0700

    created function that will take user input and exit once enter is hit without typing a name

--- Program.cs
+++ Program.cs
@@ -8,10 +8,19 @@ namespace CatWorx.BadgeMaker
         // Main must be nested inside a class Only one entry point
         static void Main(string[] args) // Entry Point to application
         {
-            List<string> employees = new List<string>(){ "adam", "amy" };
-
-            employees.Add("barbara");
-            employees.Add("billy");
+            List<string> employees = new List<string>();
+            // Collect user values until the value is an empty string
+            while (true)
+            {
+                Console.WriteLine("Please enter a name: ");
+                string input = Console.ReadLine();
+                // Break if the user hits ENTER without typing a name
+                if (input == "")
+                {
+                    break;
+                }
+                employees.Add(input);
+            }
             for (int i = 0; i < employees.Count; i++)
             {
                 Console.WriteLine(employees[i]);
