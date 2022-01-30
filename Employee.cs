7067de285e29c8727573f08a47bfe5c6c1ab3dfa
rjhelm <rjhelm@github.io>
2022-01-29 19:13:49 -0700

    changed functions to print full employee class when all paramaters are entered in console

--- Employee.cs
+++ Employee.cs
@@ -1,10 +1,10 @@
 namespace CatWorx.BadgeMaker
 {
     class Employee{ 
-        public string FirstName;
-        public string LastName;
-        public int Id;
-        public string PhotoUrl;
+        private string FirstName;
+        private string LastName;
+        private int Id;
+        private string PhotoUrl;
         public Employee(string firstName, string lastName, int id, string photoUrl) {
             FirstName = firstName;
             LastName = lastName;
@@ -13,6 +13,12 @@ namespace CatWorx.BadgeMaker
         }
         public string GetName() {
             return FirstName + " " + LastName;
+        }
+        public int GetId() {
+            return Id;
+        }
+        public string GetPhotoUrl() {
+            return PhotoUrl;
         }
     }
 }
